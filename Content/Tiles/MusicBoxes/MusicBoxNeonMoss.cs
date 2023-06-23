4cc51a4dfee0df7a7efed8a2fa447a970f135331
DarkMango <117542029+DarkWinters@users.noreply.github.com>
2023-06-23 16:39:15 -0600

    Music Boxes (Ebon please fix Starfall lol)
    Starfall currently plays a temporary file because I'm too stupid to figure out how to work out looped tracks.

---
+++ Content/Tiles/MusicBoxes/MusicBoxNeonMoss.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Poganka.Content.Tiles.MusicBoxes
+{
+    public class MusicBoxNeonMoss : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.CanGetPrefixes[Type] = false;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.MusicBox;
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Assets/Music/GlowingMoss"), ModContent.ItemType<MusicBoxNeonMoss>(), ModContent.TileType<MusicBoxNeonMossTile>());
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToMusicBox(ModContent.TileType<MusicBoxNeonMossTile>(), 0);
+        }
+    }
+
+    public class MusicBoxNeonMossTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.newTile.StyleLineSkip = 2;
+
+            TileObjectData.addTile(Type);
+
+            LocalizedText name = CreateMapEntryName();
+
+            AddMapEntry(new Color(200, 200, 200), name);
+
+            DustType = -1;
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<MusicBoxNeonMoss>();
+        }
+    }
+}
---
+++ Content/Tiles/MusicBoxes/MusicBoxStarfall.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Poganka.Content.Tiles.MusicBoxes
+{
+    public class MusicBoxStarfall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.CanGetPrefixes[Type] = false;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.MusicBox;
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Assets/Music/Starfall"), ModContent.ItemType<MusicBoxStarfall>(), ModContent.TileType<MusicBoxStarfallTile>());
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToMusicBox(ModContent.TileType<MusicBoxStarfallTile>(), 0);
+        }
+    }
+
+    public class MusicBoxStarfallTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.newTile.StyleLineSkip = 2;
+
+            TileObjectData.addTile(Type);
+
+            LocalizedText name = CreateMapEntryName();
+
+            AddMapEntry(new Color(200, 200, 200), name);
+
+            DustType = -1;
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<MusicBoxStarfall>();
+        }
+    }
+}
